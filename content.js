95c778c61e4991db8673ff69b500c1452b972705
Linda Wang <61102300+ylw311@users.noreply.github.com>
2023-07-31 21:39:57 -0400

    allow specific site blocking

--- content.js
+++ content.js
@@ -29,3 +29,41 @@ function isNotInboxUrl() {
   });
   
 
+
+
+
+// blocking site access
+const websitesBlocked = ["youtube", "reddit"]; 
+
+function isBlockedWebsite(url) {
+  return websitesBlocked.some((blocked) => url.includes(blocked));
+}
+
+function createBlackoutOverlay() {
+    const overlay = document.createElement("div");
+    overlay.id = "distraction-blocker-overlay";
+    overlay.innerText = "Yo, getting distracted, go back to work >:(";
+    overlay.style.position = "fixed";
+    overlay.style.top = "0";
+    overlay.style.left = "0";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
+    overlay.style.color = "white";
+    overlay.style.fontFamily = "Arial, sans-serif";
+    overlay.style.fontSize = "24px";
+    overlay.style.display = "flex";
+    overlay.style.justifyContent = "center";
+    overlay.style.alignItems = "center";
+    document.body.appendChild(overlay);
+  }
+  
+  
+function handlePageLoad() {
+  if (isBlockedWebsite(window.location.href)) {
+    createBlackoutOverlay();
+  }
+}
+
+window.addEventListener("load", handlePageLoad);
+
